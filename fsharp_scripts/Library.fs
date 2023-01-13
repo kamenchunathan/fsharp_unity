74dc277b01f23858099af7e234dd0557d5e38b62
Nathan Kamenchu <kamenchunathan@gmail.com>
2023-01-13 23:33:15 +0300

    init: Simple project that gets Unity working with F#
    Contains a single script that instantiates a number of prefab
    instances

---
+++ fsharp_scripts/Library.fs
@@ -0,0 +1,39 @@
+﻿namespace fsharp_scripts
+
+open UnityEngine
+
+
+type Card = { flipped: bool; value: int }
+
+
+type CardManager() =
+    inherit MonoBehaviour()
+
+    let mutable cardOjbects: GameObject[] = [||]
+
+    let mutable cards: Card[] =
+        [ for i in 1..4 ->
+              { flipped = (if i % 2 = 0 then true else false)
+                value = i } ]
+        |> Array.ofList
+
+    [<field: SerializeField>]
+    member val public CardPrefab: GameObject = null with get, set
+
+    member this.Cards = cards
+    member val CardObjects = cardOjbects with get, set
+
+
+    member this.Start() =
+        this.CardObjects <-
+            Array.mapi
+                (fun i _ ->
+                    GameObject.Instantiate(
+                        this.CardPrefab,
+                        Vector3(float32 i, float32 0.0, float32 0.0),
+                        Quaternion.identity
+                    )
+                    :?> GameObject)
+                cards
+
+    member this.Update() = ()
